4389fc76fa104ad8bc2234abd2dc861e54274f27
stegben <stegben.benjamin@gmail.com>
2020-04-09 16:14:16 +0800

    init impl

---
+++ segdtw/distance.py
@@ -0,0 +1,105 @@
+from collections import defaultdict
+import numbers
+
+import numpy as np
+
+
+def __difference(a, b):
+    return abs(a - b)
+
+
+def __norm(p):
+    return lambda a, b: np.linalg.norm(np.atleast_1d(a) - np.atleast_1d(b), p)
+
+
+def segdtw(source, query, radius, dist):
+    assert source.shape[1] == query.shape[1]
+    source, query, dist = __prep_inputs(source, query, dist)
+    s_len = source.shape[0]
+    q_len = query.shape[0]
+
+    n_bands = (s_len - q_len) // radius
+
+    distances = []
+    paths = []
+    for band_idx in range(n_bands):
+        s_start = radius * band_idx
+        s_end = s_start + q_len + radius
+
+        window = __expand_window([(i, i) for i in range(q_len)], s_end - s_start, q_len,radius)
+
+        distance, path = __dtw(source[s_start:s_end], query, window=window, dist=dist)
+        distances.append(distance)
+        paths.append([(s_start + x, y)for x, y in path])
+
+    return distances, paths
+
+
+def __prep_inputs(x, y, dist):
+    x = np.asanyarray(x, dtype='float')
+    y = np.asanyarray(y, dtype='float')
+
+    if x.ndim == y.ndim > 1 and x.shape[1] != y.shape[1]:
+        raise ValueError('second dimension of x and y must be the same')
+    if isinstance(dist, numbers.Number) and dist <= 0:
+        raise ValueError('dist cannot be a negative integer')
+
+    if dist is None:
+        if x.ndim == 1:
+            dist = __difference
+        else:
+            dist = __norm(p=2)
+    elif isinstance(dist, numbers.Number):
+        dist = __norm(p=dist)
+
+    return x, y, dist
+
+
+def __dtw(x, y, window, dist):
+    len_x, len_y = len(x), len(y)
+    if window is None:
+        window = [(i, j) for i in range(len_x) for j in range(len_y)]
+    window = ((i + 1, j + 1) for i, j in window)
+    D = defaultdict(lambda: (float('inf'),))
+    D[0, 0] = (0, 0, 0)
+    for i, j in window:
+        dt = dist(x[i-1], y[j-1])
+        D[i, j] = min((D[i-1, j][0]+dt, i-1, j), (D[i, j-1][0]+dt, i, j-1),
+                      (D[i-1, j-1][0]+dt, i-1, j-1), key=lambda a: a[0])
+    path = []
+    i, j = len_x, len_y
+    while not (i == j == 0):
+        path.append((i-1, j-1))
+        i, j = D[i, j][1], D[i, j][2]
+    path.reverse()
+    return (D[len_x, len_y][0], path)
+
+
+def __expand_window(path, len_x, len_y, radius):
+    path_ = set(path)
+    for i, j in path:
+        for a, b in ((i + a, j + b)
+                     for a in range(-radius, radius+1)
+                     for b in range(-radius, radius+1)):
+            path_.add((a, b))
+
+    window_ = set()
+    for i, j in path_:
+        for a, b in ((i * 2, j * 2), (i * 2, j * 2 + 1),
+                     (i * 2 + 1, j * 2), (i * 2 + 1, j * 2 + 1)):
+            window_.add((a, b))
+
+    window = []
+    start_j = 0
+    for i in range(0, len_x):
+        new_start_j = None
+        for j in range(start_j, len_y):
+            if (i, j) in window_:
+                window.append((i, j))
+                if new_start_j is None:
+                    new_start_j = j
+            elif new_start_j is not None:
+                break
+        start_j = new_start_j
+
+    return window
